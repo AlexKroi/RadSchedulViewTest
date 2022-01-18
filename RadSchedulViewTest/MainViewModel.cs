1af36e0668052f30cef62f9f0f8ebe261bf42f55
Alexander Kroiss <alexander.kroiss@gmail.com>
2022-01-18 10:25:11 +0100

    Projektdateien hinzufügen.

---
+++ RadSchedulViewTest/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Windows.Controls.ScheduleView;
+using Telerik.Windows.Data;
+
+namespace RadSchedulViewTest
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            AppointmentSource = new RadObservableCollection<Appointment>
+            {
+                new Appointment(){Subject="test1",Start=DateTime.Now.Date.AddHours(7),End=DateTime.Now.Date.AddHours(10)}
+            };
+        }
+
+        public RadObservableCollection<Appointment> AppointmentSource { get; set; }        
+    }
+}
